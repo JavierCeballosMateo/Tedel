18435550d147b8f5b623f7ae74cd9629ce30393f
javierceballos <jcebmat@gmail.com>
2023-05-19 14:24:24 +0200

    Hecha una parte del LoignActivity y el RegisterActivity con sus respectivos xml

--- app/src/main/java/com/example/tedel/activities/LoginActivity.java
+++ app/src/main/java/com/example/tedel/activities/LoginActivity.java
@@ -1,5 +1,47 @@
 package com.example.tedel.activities;
 
-public class LoginActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.tedel.R;
+
+public class LoginActivity extends Activity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        username = findViewById(R.id.loginUsername);
+        password = findViewById(R.id.loginPassword);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textUsername = username.getText().toString();
+                String textPassword = password.getText().toString();
+
+                if (checkLogin(textUsername, textPassword)){
+                    Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean checkLogin(String textUsername, String textPassword){
+        return textUsername.equals(textUsername) && textPassword.equals(textPassword);
+
+    }
 
 }
--- app/src/main/java/com/example/tedel/activities/RegisterActivity.java
+++ app/src/main/java/com/example/tedel/activities/RegisterActivity.java
@@ -1,5 +1,49 @@
 package com.example.tedel.activities;
 
-public class RegisterActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.tedel.R;
+
+public class RegisterActivity extends Activity {
+
+    private EditText username;
+    private EditText password;
+    private EditText confirmPassword;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+
+        username = findViewById(R.id.registerUsername);
+        password = findViewById(R.id.registerPassword);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textUsername = username.getText().toString();
+                String textPassword = password.getText().toString();
+                String textConfirmPassword = confirmPassword.getText().toString();
+
+                if(checkRegister(textUsername,textPassword,textConfirmPassword)){
+                    Toast.makeText(RegisterActivity.this, "Registrado con éxito", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(RegisterActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean checkRegister(String textUsername, String textPassword, String textConfirmPassword){
+        //Verifico si la contraseña y el confirmarContraseña coinciden
+        return textPassword.equals(textConfirmPassword);
+    }
 }
